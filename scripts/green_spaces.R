ba84624e100b74c90f728d2fe0fa6a32761591d6
Garland Xie <garlandxie@gmail.com>
2022-08-26 12:26:15 -0400

    Add more geospatial analyses

--- scripts/green_spaces.R
+++ scripts/green_spaces.R
@@ -1,27 +1,48 @@
 # libraries ----
-library(opendatatoronto)
 library(dplyr)
+library(sf)
+library(here)
 
-# get package ----
-package <- show_package("9a284a84-b9ff-484b-9e30-82f22c1780b9")
+# import ----
 
-# get all resources for this package
-resources <- list_package_resources("9a284a84-b9ff-484b-9e30-82f22c1780b9")
+# green space sizes
+to_green_space <- st_read(
+  here("data", "green_space_shp", "Green Spaces.shp")
+)
 
-# identify data-store resources
-# by default, Toronto Open Data sets data-store resource format to CSV 
-# for non-geospatial and GeoJSON for geospatial resources
+# metadata
+field_codes <- read.csv(
+  here("data", "green_space_shp", "Green Spaces_fields.csv")
+)
 
-datastore_resources <- filter(
-  resources, 
-  tolower(format) %in% c('csv', 'geojson')
-  )
+# merge polygons ----
 
-# download CSV of green spaces with the projected coordinate system
-# MTM 10, EPSG 2952
-to_green_spaces <- filter(
-  datastore_resources, 
-  name == "Green Spaces - 2952.csv") %>% 
-  get_resource()
+to_merge <- to_green_space %>%
+  
+  # renaming columns based on Green Spaces_field.csv file
+  rename(
+    id = FIELD_1,
+    area_id = FIELD_2, 
+    area_attr_id = FIELD_3, 
+    parent_area_id = FIELD_4, 
+    area_class_id = FIELD_5, 
+    area_class = FIELD_6, 
+    area_short_code = FIELD_7, 
+    area_long_code = FIELD_8, 
+    area_name = FIELD_9, 
+    area_desc = FIELD_10, 
+    objectid = FIELD_11
+  ) %>%
+  
+  # merge nested and duplicated green spaces
+  # to form a contiguous polygon
+  group_by(area_name) %>%
+  summarize(geometry = sf::st_union(geometry)) %>%
+  ungroup()
 
-
+# export as ESRI shape file ----
+st_write(
+  obj = to_merge, 
+  dsn = here("data", "merged_green_spaces.shp"),
+  delete_dsn = TRUE
+)
